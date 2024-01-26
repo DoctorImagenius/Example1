64b1d8a0143560691fa9659eda473362b898ff27
DoctorImagenius <imagenius1001@gmail.com>
2024-01-26 18:34:41 +0500

    This is dev2 branch after changing all reviews...

--- javascript.js
+++ javascript.js
@@ -1,7 +1,9 @@
 var screen = document.querySelector("#screen");
 var btn = document.querySelectorAll(".btn");
-let varNameArray = [];
-let varValueArray = [];
+let names = [];
+let values = [];
+let showHistory = 0;
+let ShowVar = 0;
 for (item of btn) {
     item.addEventListener("click", (e) => {
         text = e.target.innerText;
@@ -14,19 +16,17 @@ for (item of btn) {
         screen.value += text;
     });
 }
-function result() {
+
+function ShowResult() {
     let exp = null;
     let res = null;
     exp = screen.value;
     try {
-        for (var j = 0; j < screen.value.length; j++) {
-            for (var i = 0; i < varNameArray.length; i++) {
-                screen.value = screen.value.replace(
-                    `${varNameArray[i]}`,
-                    `${varValueArray[i]}`
-                );
-            }
+        for (let i = 0; i < names.length; i++) {
+            const regex = new RegExp(`${names[i]}`, "g");
+            screen.value = screen.value.replace(regex, `${values[i]}`);
         }
+
         screen.value = screen.value.replace(/sin/g, "Math.sin");
         screen.value = screen.value.replace(/cos/g, "Math.cos");
         screen.value = screen.value.replace(/tan/g, "Math.tan");
@@ -34,12 +34,14 @@ function result() {
         function pow(x = 1, y = 1) {
             return x ** y;
         }
+
         screen.value = screen.value.replace(/p/g, "pow");
         for (var i = 0; i < screen.value.length; i++) {
             var piIndex = screen.value.indexOf("π");
             if (piIndex == 0) {
                 screen.value = screen.value.replace("π", "3.1415");
             }
+
             if (piIndex !== -1 && piIndex > 0) {
                 var characterBeforePi = screen.value[piIndex - 1];
                 if (
@@ -55,11 +57,13 @@ function result() {
                 }
             }
         }
+
         for (var i = 0; i < screen.value.length; i++) {
             var eIndex = screen.value.indexOf("e");
             if (eIndex == 0) {
                 screen.value = screen.value.replace("e", "2.7182");
             }
+
             if (eIndex !== -1 && eIndex > 0) {
                 var characterBeforee = screen.value[eIndex - 1];
                 if (
@@ -74,78 +78,80 @@ function result() {
                 }
             }
         }
+
         screen.value = eval(screen.value)
             .toFixed(4)
             .replace(/\.?0+$/, "");
-
         if (eval(screen.value) == "Infinity") {
             screen.value = "Answer is Infinity...";
         }
+
         if (screen.value == "NaN") {
             screen.value = "Incomplete Expression...";
         }
+
         res = screen.value;
     } catch (error) {
         screen.value = "Expression Error!";
         res = screen.value;
     }
+
     addButton(exp, res);
 }
-function addButton(e, r) {
-    const bt = document.createElement("button");
-    bt.classList.add("historyButton");
-    bt.innerHTML = `${e} = ${r} <button class='delhistory'>Delete</button>`;
-    bt.addEventListener("click", (e) => {
+
+function addButton(Exp, Res) {
+    const button = document.createElement("button");
+    button.classList.add("historyButton");
+    button.innerHTML = `${Exp} = ${Res} <button class='delhistory'>Delete</button>`;
+    button.addEventListener("click", (e) => {
         expression = e.target.innerText;
-        let strVar = expression.split("=")[0];
-        if (strVar == "Delete") {
-            bt.style.display = "none";
+        let deleteValue = expression.split("=")[0];
+        if (deleteValue == "Delete") {
+            button.remove();
             return;
         }
-        screen.value = strVar;
+        screen.value = deleteValue;
     });
-    bt.style.display = "flex";
-    bt.style.justifyContent = "space-between";
-    bt.style.alignItems = "center";
+    button.classList.add("ModifyButtons");
+    herDiv = document.getElementsByClassName("history")[0];
+    herDiv.appendChild(button);
+}
 
-    hisDiv = document.getElementsByClassName("history")[0];
-    hisDiv.appendChild(bt);
+function ShowHistory() {
+    showHistory == 0
+        ? ((document.getElementById("history").style.display = "flex"),
+          (showHistory = 1))
+        : ((document.getElementById("history").style.display = "none"),
+          (showHistory = 0));
 }
-let showHistory = 0;
-function history() {
-    if (showHistory == 0) {
-        document.getElementById("history").style.display = "flex";
-        showHistory = 1;
-    } else {
-        document.getElementById("history").style.display = "none";
-        showHistory = 0;
-    }
-}
-let ShowVar = 0;
+
 function ShowVariable() {
-    if (ShowVar == 0) {
-        document.getElementById("variable").style.display = "flex";
-        ShowVar = 1;
-    } else {
-        document.getElementById("variable").style.display = "none";
-        ShowVar = 0;
-    }
+    ShowVar == 0
+        ? ((document.getElementById("variable").style.display = "flex"),
+          (ShowVar = 1))
+        : ((document.getElementById("variable").style.display = "none"),
+          (ShowVar = 0));
 }
-function createVar(e, r) {
-    const bt = document.createElement("button");
-    bt.classList.add("historyButton");
-    bt.innerText = `${e} = ${r}`;
-    bt.addEventListener("click", (e) => {
+
+function createVar(Exp, Res) {
+    const button = document.createElement("button");
+    button.classList.add("historyButton");
+    button.innerText = `${Exp} = ${Res}`;
+    button.addEventListener("click", (e) => {
         expression = e.target.innerText;
-        let strVar = expression.split("=")[0];
-        screen.value += strVar;
+        let addVar = expression.split("=")[0];
+        screen.value += addVar;
     });
-    hisDiv = document.getElementById("variableOutputs");
-    hisDiv.appendChild(bt);
+    herDiv = document.getElementById("variableOutputs");
+    herDiv.appendChild(button);
 }
+
 function addVar() {
     let name = document.getElementById("varName").value;
     let val = document.getElementById("varValue").value;
+    if (name == "e") {
+        val = "2.7182";
+    }
     if (val == "e") {
         val = "2.7182";
     }
@@ -156,40 +162,51 @@ function addVar() {
         screen.value = "This is constant!";
         return;
     }
-    if (/^\d+$/.test(val)) {
-        console.log("myString is a number");
-    } else {
+    if (/\d/.test(name)) {
+        screen.value = "Incorrect name!";
+        return;
+    }
+    if (!/^\d+$/.test(val)) {
         if (val != "2.7182" && val != "3.1415") {
             screen.value = "Incorrect value!";
             return;
         }
+    } else {
+        screen.value = "";
     }
-    const index = varNameArray.indexOf(name);
+    const index = names.indexOf(name);
     document.getElementById("varName").value = "";
     document.getElementById("varValue").value = "";
-
     if (index !== -1) {
-        console.log(`Element 30 is found at index ${index}`);
-        varValueArray[index] = val;
+        values[index] = val;
         createVar(name, val);
     } else {
-        varNameArray.push(name);
-        varValueArray.push(val);
+        names.push(name);
+        values.push(val);
         createVar(name, val);
     }
 }
+
+function allClear() {
+    screen.value = "";
+}
+
 function sqrt() {
     screen.value += "√()";
 }
+
 function power() {
     screen.value += "p(,)";
 }
-function sin() {
+
+function getSin() {
     screen.value += "sin()";
 }
+
 function cos() {
     screen.value += "cos()";
 }
+
 function tan() {
     screen.value += "tan()";
 }
